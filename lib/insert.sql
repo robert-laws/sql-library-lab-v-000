21fb8992977db1924dbe98f34795ff753784499f
Robert Laws <roblaws@me.com>
2017-10-08 17:30:26 +0000

    automatically backed up by learn

--- lib/insert.sql
+++ lib/insert.sql
@@ -7,3 +7,5 @@ INSERT INTO characters (name, motto, species, author_id, series_id) VALUES ('Val
 INSERT INTO subgenres (name) VALUES ('Fantasy'), ('True Crime');
 
 INSERT INTO authors (name) VALUES ('Hal Hope'), ('Bob Cobb');
+
+INSERT INTO character_books (character_id, book_id) VALUES (1, 1), (1, 2), (1, 3), (2, 1), (2, 2), (2, 3), (3, 1), (4, 2), (5, 4), (5, 5), (5, 6), (6, 4), (6, 5), (6, 6), (7, 4), (8, 6);
