94db9c14fb36474be084c8cc26da63f3799c308d
Robert Laws <roblaws@me.com>
2017-10-08 17:30:04 +0000

    automatically backed up by learn

--- lib/insert.sql
+++ lib/insert.sql
@@ -5,3 +5,5 @@ INSERT INTO books (title, year, series_id) VALUES ('Moon in the Galaxy', 2012, 1
 INSERT INTO characters (name, motto, species, author_id, series_id) VALUES ('Val', 'Howdy Folks', 'bug', 1, 1), ('Tad', 'Fun time to be Alive', 'bird', 1, 1), ('Zak', 'Going Places', 'fish', 1, 1), ('Wal', 'Stay Cool', 'reptile', 1, 1), ('Al', 'Moving On', 'human', 2, 2), ('Greg', 'Nice Place', 'bird', 2, 2), ('Bill', 'Keep Going', 'bug', 2, 2), ('Don', 'Need Some Space', 'fish', 2, 2);
 
 INSERT INTO subgenres (name) VALUES ('Fantasy'), ('True Crime');
+
+INSERT INTO authors (name) VALUES ('Hal Hope'), ('Bob Cobb');
