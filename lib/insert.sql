bbf84a023366123c2bedb05b2afd8842a27defec
Robert Laws <roblaws@me.com>
2017-10-08 17:28:59 +0000

    automatically backed up by learn

--- lib/insert.sql
+++ lib/insert.sql
@@ -1,1 +1,3 @@
 INSERT INTO series (title, author_id, subgenre_id) VALUES ('Galaxy Travels', 1, 1), ('World Ocean', 2, 1);
+
+INSERT INTO books (title, year, series_id) VALUES ('Moon in the Galaxy', 2012, 1), ('Comet Travels', 2011, 1), ('Star Hopping', 2016, 1), ('Raft to the Edge', 2010, 2), ('Sailing in the Blue', 2015, 2), ('Waterfalls', 2008, 2);
